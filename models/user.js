575c65e4e011eff4de5318a4dfffd85e052904f6
traceannwhite <traceannwhite@gmail.com>
2021-09-20 21:41:53 -0500

    backend

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const { Schema, model } = require("../db/connection"); // import Schema & model
 
 // User Schema
 const UserSchema = new Schema({
+  img: String,
+  fullName: { type: String, required: true },
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 });
